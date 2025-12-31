7740a58e1e7cbe76b04fd1c6166c9f8f7ed31c43
VanZdev <139448115+ZlaIvaDev@users.noreply.github.com>
2025-12-31 23:27:32 +0900

    version 1.0 finished, simple operation between 2 nums

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 a basic calculator with simple functionality
 on a webpage.
+
+// calculator version 1.0
+able to calculate mathematical operations between 2 numbers
+it aint much but it's an honest work eh
+
+// NOTE : Brackets are not usable, no need for them for two numbers, amirite ? 
--- javascript.js
+++ javascript.js
@@ -3,7 +3,16 @@
 
 const input = document.querySelector('input');
 const btn = document.querySelectorAll('button');
-let expression = ''
+const rez = document.querySelector('p');
+
+//let expression = '';
+let tempNum = '';
+
+let problem = {
+    prviBroj : 0,
+    operater : '',
+    drugiBroj : 0,
+}
 
 for(let b of btn) {
     b.addEventListener(
@@ -25,27 +34,70 @@ for(let b of btn) {
             case '7':
             case '8':
             case '9':
+            case '.':
+                input.value += b.textContent;
+                tempNum += b.textContent;
+                expression = input.value;
+                break;
             case '/':
             case 'x':
             case '-':
             case '+':
-                input.value += b.textContent
-                expression = input.value
+                tempNum += b.textContent;
+                //console.log(`tempnum je ${tempNum}`)
+                problem.operater = tempNum.at(-1)
+                //console.log(`operater je ${problem.operater}`)
+                problem.prviBroj = Number(tempNum.slice(0,-1));
+                //console.log(`prvi broj je ${problem.prviBroj}`)
+                tempNum = '';
+                input.value += b.textContent;
+                //problem.operater = b.textContent;
+                //expression = input.value
                 break;
             case 'AC':
-                input.value = ''
-                expression = input.value
+                input.value = '';
+                //expression = input.value;
+                rez.textContent = '';
+                tempNum = '';
+                problem.prviBroj = 0;
+                problem.drugiBroj = 0;
+                problem.operater = '';
                 break;
             case '=':
-                console.log(expression.split(''))
-                console.log(expression)
+                console.table(problem)
+                problem.drugiBroj = Number(tempNum);
+                tempNum = '';
+
+                if (problem.operater == '/') {
+                    let kalk = problem.prviBroj / problem.drugiBroj;
+                    
+                    rez.textContent = `solution : ${problem.prviBroj}/${problem.drugiBroj}=${kalk}`
+                } else if (problem.operater == 'x') {
+                    let kalk = problem.prviBroj * problem.drugiBroj;
+                    
+                    rez.textContent = `solution : ${problem.prviBroj}x${problem.drugiBroj}=${kalk}`
+                } else if (problem.operater == '-') {
+                    let kalk = problem.prviBroj - problem.drugiBroj;
+                    
+                    rez.textContent = `solution : ${problem.prviBroj}-${problem.drugiBroj}=${kalk}`
+                } else {
+                    let kalk = problem.prviBroj + problem.drugiBroj;
+                    
+                    rez.textContent = `solution : ${problem.prviBroj}+${problem.drugiBroj}=${kalk}`
+                }
+                input.value = ''
+                
+
+
                 break;
             
             default:
-                input.value += 'unknown'
+                input.value += 'invalid value bro'
                 break;
 
         }}
     )
 }
+
+
 
