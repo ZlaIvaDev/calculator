389566115a262072561bbac6554a2898c4d86044
VanZdev <139448115+ZlaIvaDev@users.noreply.github.com>
2025-12-31 19:36:26 +0900

    connected buttons to input

--- javascript.js
+++ javascript.js
@@ -1,12 +1,51 @@
+
+
+
 const input = document.querySelector('input');
 const btn = document.querySelectorAll('button');
+let expression = ''
 
 for(let b of btn) {
     b.addEventListener(
-        'click',
-        (e) => input.textContent = b.textContent
-        
+        'click', 
+        (e) => 
+            
+            {
+
+            console.log('clicked', b.textContent)
+            switch(b.textContent) {
+            
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '/':
+            case 'x':
+            case '-':
+            case '+':
+                input.value += b.textContent
+                expression = input.value
+                break;
+            case 'AC':
+                input.value = ''
+                expression = input.value
+                break;
+            case '=':
+                console.log(expression.split(''))
+                console.log(expression)
+                break;
+            
+            default:
+                input.value += 'unknown'
+                break;
+
+        }}
     )
 }
-
 
